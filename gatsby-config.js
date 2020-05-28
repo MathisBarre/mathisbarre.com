c744a870198d2b739d38d7902a9db2df08c2eb4c
Mathis Barré <mathis.barre@live.fr>
2020-05-28 10:08:50 +0200

    feat: google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,23 @@ module.exports = {
   //   baseurl: "https://mathisbarre.com",
   // },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-139419835-3",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: true
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
